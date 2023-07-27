744e5dc42e6a529da92a1c3fa83243bb65fc37f1
victus <wwwranebrijesh254@gmail.com>
2023-07-27 19:59:30 +0530

    updated to quiz questions design

--- app/src/main/java/com/example/quiizapp/MainActivity2.java
+++ app/src/main/java/com/example/quiizapp/MainActivity2.java
@@ -1,29 +1,41 @@
 package com.example.quiizapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
 public class MainActivity2 extends AppCompatActivity {
 
     FirebaseAuth mAuth;
-    Button btnLogout;
+    //Button btnLogout;
     TextView userDetails;
     FirebaseUser user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        ListView myListView = findViewById(R.id.myListView);
+
         mAuth = FirebaseAuth.getInstance();
-        btnLogout = findViewById(R.id.btnLogout);
         userDetails = findViewById(R.id.userDetails);
         user = mAuth.getCurrentUser();
         if (user == null)
@@ -36,14 +48,44 @@ public class MainActivity2 extends AppCompatActivity {
         {
             userDetails.setText(user.getEmail());
         }
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+        ArrayList<String> QuizList = new ArrayList<>();
+        QuizList.add("C/C++");
+        QuizList.add("Java");
+        QuizList.add("Python");
+        QuizList.add("Android");
+        QuizList.add("Javascript");
+        ArrayAdapter<String> Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,QuizList);
+
+        myListView.setAdapter(Adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-                finish();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String text = "Opening " + i + " " + ((TextView) view).getText().toString();
+                Toast.makeText(MainActivity2.this,text + " Clicked", Toast.LENGTH_LONG).show();
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.item1)
+        {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
+            finish();
+            Toast.makeText(this, "Account Logged out!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
